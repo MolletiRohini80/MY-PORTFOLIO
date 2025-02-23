ed2d222ed13f7385945a5a3eab5dabdc2bec1011
Molleti Rohini <119840376+MolletiRohini80@users.noreply.github.com>
2025-02-23 15:29:42 +0530

    Update Works.tsx

--- app/components/work-section/Works.tsx
+++ app/components/work-section/Works.tsx
@@ -15,7 +15,7 @@ export default function Works() {
     {
       title: "Bus-Tracking Application",
       gitLink: "https://github.com/MolletiRohini80/BUS-IN-OUT.git",
-      liveLink: "https://offsetcomms.africa/",
+      // liveLink: "https://offsetcomms.africa/",
       about:
         "Bus tracking project is a real-time system built with ReactJS and Vite, designed to track bus arrivals and departures at your college gate. It includes a student count graph and a pie chart for data visualization. The project features a responsive design, optimized for various screen sizes, with smooth animations and a clean, scroll-free UI. It highlights your skills in full-stack development, responsive design, and data visualization.",
       stack: ["React", "JavaScript", "NodeJS", "ExpressJS","MERN"],
